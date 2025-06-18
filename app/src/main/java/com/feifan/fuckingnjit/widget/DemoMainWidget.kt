c63311c2fe75b6ba9dc0ab33cf0a2cd0c0e337eb
OneFeiFan <3022504358@qq.com>
2025-06-18 17:43:16 +0800

    删除了无用的小部件方法，，修改并添加了自动创建小部件的方法

--- app/src/main/java/com/feifan/fuckingnjit/widget/DemoMainWidget.kt
+++ app/src/main/java/com/feifan/fuckingnjit/widget/DemoMainWidget.kt
@@ -6,55 +6,52 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.RemoteViews
-import android.widget.Toast
 import com.feifan.fuckingnjit.R
 import com.feifan.fuckingnjit.utils.XiaomiUtilities
 
 class DemoMainWidget (val context: Context){
-    fun createWidget() {
-//        if (data == null||data.isEmpty()) {
-//            Toast.makeText(context, "输入内容为空，无法创建组件", Toast.LENGTH_SHORT).show()
-//            return
-//        }
 
+    fun getPermission() {
+        val intent = XiaomiUtilities.getPermissionManagerIntent(context).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        context.startActivity(intent)
+    }
+
+    fun isWidgetAlreadyCreated(): Boolean {
+        val widgetIDs = AppWidgetManager.getInstance(context)
+            .getAppWidgetIds(ComponentName(context, DemoWidgetProvider::class.java))
+        return widgetIDs.isNotEmpty()
+    }
+
+
+    fun createWidget():String {
         val appWidgetManager = AppWidgetManager.getInstance(context)
         val provider = ComponentName(context, DemoWidgetProvider::class.java)
 
         if (!appWidgetManager.isRequestPinAppWidgetSupported) {
-            Toast.makeText(context, "桌面不支持自动添加小部件，请手动添加", Toast.LENGTH_SHORT).show()
-            return
+            return """{"state":"error","message":"桌面不支持自动添加小部件，请手动添加。"}"""
         }
         // 检查是否是小米手机
         if (XiaomiUtilities.isMIUI) {
             // 检查是否已授予自定义权限
             if (!XiaomiUtilities.isCustomPermissionGranted(context, XiaomiUtilities.OP_INSTALL_SHORTCUT)) {
-                // 创建并显示AlertDialog
-                // 启动透明 Activity 托管对话框
-                val intent = Intent(context, DialogHostActivity::class.java).apply {
-                    putExtra("title", "快捷方式权限申请")
-                    putExtra("message", "操作失败！！！为了固定小部件，需要授予“桌面快捷方式”权限。是否现在授予权限？")
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK // 关键标志
-                }
-                context.startActivity(intent)
-                return // 确保在用户授予权限之前不再执行后续代码
+                return """{"state":"need_permission","message":""}"""
             }
         }
-        val widgetIDs = AppWidgetManager.getInstance(context)
-            .getAppWidgetIds(ComponentName(
-                context,
-                DemoWidgetProvider::class.java.getName()
-            ))
         //检测是否已经创建过小部件
-        if (widgetIDs.isNotEmpty()) {
-            Toast.makeText(context, "只能创建一个小部件", Toast.LENGTH_SHORT).show()
-            return
+        if (isWidgetAlreadyCreated()) {
+            return """{"state":"error","message":"只能创建一个小部件"}"""
         }
         val remoteViews = RemoteViews(context.packageName, R.layout.widget)
         val bundle = Bundle()
         bundle.putParcelable(AppWidgetManager.EXTRA_APPWIDGET_PREVIEW, remoteViews)
 
         if(!appWidgetManager.requestPinAppWidget(provider, bundle, null)){
-            Toast.makeText(context,"小部件大概率创建失败了，可能是你的桌面不支持？？？或者没有快捷方式权限？？？", Toast.LENGTH_SHORT).show()
+            return """{"state":"error","message":"小部件大概率创建失败了，可能是你的桌面不支持？？？或者没有快捷方式权限？？？"}"""
         }
+
+        return """{"state":"success","message":"请返回桌面，自行查看小部件是否创建成功。"}"""
     }
+
 }
